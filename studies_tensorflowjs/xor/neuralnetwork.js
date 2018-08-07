166928bc4d68190c8c2181b21804c216627cf5c9
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-06 23:08:45 -0300

    Adding Tensorflow.js initial example - copied from @Shiffman

---
+++ studies_tensorflowjs/xor/neuralnetwork.js
@@ -0,0 +1,116 @@
+// Data helper classes
+
+// class ml5.Data
+
+// ml5.Data?
+class Data {
+  // Need to deal with shape
+  constructor(data) {
+    this.xs = tf.tensor2d(data.inputs);
+    this.ys = tf.tensor2d(data.targets);
+  }
+}
+
+// Helper class for a Batch of Data: ml5.Batch
+class Batch {
+  constructor() {
+    // Need to deal with shape
+    // this.shape = ??;
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+  }
+
+  toTensor() {
+    this.data = tf.tensor2d(this.data);
+  }
+}
+
+class NeuralNetwork {
+  constructor(inputs, hidden, outputs, lr) {
+    this.model = tf.sequential();
+    const hiddenLayer = tf.layers.dense({
+      units: hidden,
+      inputShape: [inputs],
+      activation: 'sigmoid'
+    });
+    const outputLayer = tf.layers.dense({
+      units: outputs,
+      // inferred
+      // inputShape: [hidden],
+      activation: 'sigmoid'
+    });
+    this.model.add(hiddenLayer);
+    this.model.add(outputLayer);
+
+    const LEARNING_RATE = lr || 0.5;
+    const optimizer = tf.train.sgd(LEARNING_RATE);
+
+    this.model.compile({
+      optimizer: optimizer,
+      // optimizer: 'sgd',
+      loss: 'meanSquaredError'
+    });
+  }
+
+  predict(inputs) {
+    return tf.tidy(() => {
+      let data;
+      if (inputs instanceof Batch) {
+        data = inputs.data;
+      } else {
+        data = [inputs];
+      }
+      const xs = data instanceof tf.Tensor ? data : tf.tensor2d(data);
+      return this.model.predict(xs).dataSync();
+    });
+  }
+
+  async predictAsync(inputs) {
+    let ys = tf.tidy(() => {
+      let data;
+      if (inputs instanceof Batch) {
+        data = inputs.data;
+      } else {
+        data = [inputs];
+      }
+      const xs = data instanceof tf.Tensor ? data : tf.tensor2d(data);
+      return this.model.predict(xs);
+    });
+    let res = await ys.data();
+    ys.dispose();
+    return res;
+  }
+
+  setTrainingData(data) {
+    if (data instanceof Data) {
+      this.trainingData = data;
+    } else {
+      this.trainingData = new Data(data);
+    }
+  }
+
+  async train(epochs, data) {
+    let xs, ys;
+    if (data) {
+      xs = tf.tensor2d(data.inputs);
+      ys = tf.tensor2d(data.targets);
+    } else if (this.trainingData) {
+      xs = this.trainingData.xs;
+      ys = this.trainingData.ys;
+    } else {
+      console.log("I have no data!");
+      return;
+    }
+    await this.model.fit(xs, ys, {
+      epochs: epochs || 1,
+      shuffle: true
+    });
+    if (data) {
+      xs.dispose();
+      ys.dispose();
+    }
+  }
+}
---
+++ studies_tensorflowjs/xor/sketch.js
@@ -0,0 +1,88 @@
+/* The training dataset is so small that duplicating the data would speed up training
+   more efficiently than increasing the number of epochs. */
+let data = {
+  inputs: [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+  ],
+  targets: [
+    [0],
+    [1],
+    [1],
+    [0]
+  ]
+};
+
+let resolution = 50;
+let batch;
+let results;
+let cols;
+let rows;
+
+let counter = 0;
+
+let training = false;
+
+function train() {
+  if (!training) {
+    training = true;
+    nn.train().then(finished);
+  }
+}
+
+function finished() {
+  counter++;
+  statusP.html('training pass: ' + counter + '<br>framerate: ' + floor(frameRate()));
+  training = false;
+  nn.predictAsync(batch).then(ys => (results = ys));
+
+  // We need to let the JavaScript event loop tick forward before we call `train()`.
+  setTimeout(train, 0);
+}
+
+let statusP;
+
+function setup() {
+  createCanvas(400, 400);
+  noStroke();
+  textAlign(CENTER, CENTER);
+  statusP = createP('0');
+
+  nn = new NeuralNetwork(2, 2, 1);
+  nn.setTrainingData(data);
+
+  // The test input never changes, so we can precalculate it here.
+  batch = new Batch();
+  cols = width / resolution;
+  rows = height / resolution;
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x1 = i / cols;
+      let x2 = j / rows;
+      let inputs = [x1, x2];
+      batch.add(inputs);
+    }
+  }
+  batch.toTensor();
+  results = nn.predict(batch);
+  train();
+}
+
+function draw() {
+  // Technically, this only needs to be redrawn when `predictAsync()` is fulfilled.
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let y = results[i + j * rows];
+      fill(y * 255);
+      rect(i * resolution, j * resolution, resolution, resolution);
+      fill(255 - y * 255);
+      text(
+        nf(y, 0, 2),
+        i * resolution + resolution / 2,
+        j * resolution + resolution / 2
+      );
+    }
+  }
+}
