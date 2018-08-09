c4fa06024e63545b56c446d0dfb36137c0ac5036
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-09 07:20:37 -0300

    Creating model with tensorflow.js

--- studies_tensorflowjs/Tensors/sketch.js
+++ studies_tensorflowjs/Tensors/sketch.js
@@ -1,14 +1,40 @@
-function setup(){
-  noCanvas();
+const model = tf.sequential();
 
-  const values = [];
-  const shape = [2, 5, 3];
+const configHidden = {
+  units: 4,
+  inputShape: [2],
+  activation: 'sigmoid'
+};
+const hidden = tf.layers.dense(configHidden);
 
-  for(let i = 0; i < 30; i++){
-    values[i] = random(0, 100);
-  }
+const configOutput = {
+    units: 3,
+    inputShape: [4],
+    activation: 'sigmoid'
+};
+const output = tf.layers.dense(configOutput);
 
-  const tenso = tf.tensor(values, shape, 'int32');
 
-  tenso.print(tenso.data());
-}
+model.add(hidden);
+model.add(output);
+
+const sgdOpt = tf.train.sgd(0.1);
+const config = {
+  optimizer: sgdOpt,
+  loss: 'meanSquaredError'
+};
+model.compile(config);
+// function setup(){
+//   noCanvas();
+//
+//   const values = [];
+//   const shape = [2, 5, 3];
+//
+//   for(let i = 0; i < 30; i++){
+//     values[i] = random(0, 100);
+//   }
+//
+//   const tenso = tf.tensor(values, shape, 'int32');
+//
+//   // tenso.print(tenso.data());
+// }
