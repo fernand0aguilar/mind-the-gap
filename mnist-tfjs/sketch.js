f4d19cbf805e87116758b6f480eca0ace1115324
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-23 20:20:00 -0300

    update sketch mnist

--- mnist-tfjs/sketch.js
+++ mnist-tfjs/sketch.js
@@ -1,36 +1,46 @@
 let testData;
 let trainData;
+let model;
+
+let xs, ys;
 
 const IMG_SIZE = 784;
-
-let trainTensor, testTensor;
 
 function preload() {
   // testData = loadTable("zdata/example.csv", "csv", "header");
   trainData = loadTable("zdata/train1001.csv", 'csv', 'header');
-  // testData = await loadTable("zdata/test1.csv", 'csv', 'header');
+  // testData = loadTable("zdata/test1001.csv", 'csv', 'header');
 }
 
+function cleanData(data){
+  let values = [];
+  let labels = [];
+
+  for (let i = 0; i < data.rows.length; i++){
+    let number = data.rows[i];
+    labels.push(number.arr.shift());
+    values.push(number.arr);
+  }
+  let labelsTensor = tf.tensor1d(labels, 'int32');
+  xs = tf.tensor2d(values);
+  ys = tf.oneHot(labelsTensor, 10);
+
+  labelsTensor.dispose();
+}
+
+
+//[3,3,10] conv, [1,1,1] conv, [,256] dense, [,10] dense, batch size 56, epochs 13
+function createModel(){
+  model = tf.sequential();
+}
 
 function setup() {
   createCanvas(200, 200);
   background(0);
+  cleanData(trainData);
 
-  // console.log(trainData);
-  const CHUNK = trainData.rows.length;
-  const shape = [CHUNK, IMG_SIZE];
-
-  let values = [];
-  let labels = [];
-
-  for (let i = 0; i < CHUNK; i++) {
-    labels.push(trainData.rows[i].arr.shift());
-    values[i] = trainData.rows[i].arr;
-  }
-  const example = tf.tensor2d(values, shape, 'int32');
-  example.print();
-
-  example.dispose();
+  // const example = tf.tensor2d(values, shape, 'int32');
+  // example.dispose();
 }
 
 function draw() {
