e43281702d5dc9115ee514a254021450f3022a42
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-24 08:48:01 -0300

    blablabla

--- mnist-tfjs/sketch.js
+++ mnist-tfjs/sketch.js
@@ -21,6 +21,7 @@ function cleanData(data){
     labels.push(number.arr.shift());
     values.push(number.arr);
   }
+
   let labelsTensor = tf.tensor1d(labels, 'int32');
   xs = tf.tensor2d(values);
   ys = tf.oneHot(labelsTensor, 10);
@@ -28,10 +29,60 @@ function cleanData(data){
   labelsTensor.dispose();
 }
 
-
-//[3,3,10] conv, [1,1,1] conv, [,256] dense, [,10] dense, batch size 56, epochs 13
+//TODO -> [3,3,10] conv, [1,1,1] conv, [,256] dense, [,10] dense, batch size 56, epochs 13
 function createModel(){
   model = tf.sequential();
+
+  const hidden = tf.layers.dense({
+    inputShape: [784],
+    units: 3,
+    activation: 'sigmoid'
+  });
+
+  const output = tf.layers.dense({
+    units: 10,
+    activation: 'softmax'
+  });
+
+  model.add(hidden);
+  model.add(output);
+
+  const config = {
+    optimizer: tf.train.sgd(0.25),
+    loss: 'meanSquaredError'
+  }
+
+  model.compile(config);
+}
+
+async function trainModel(){
+  const options = {
+    epochs: 9,
+    shuffle: true,
+    batchSize: 56,
+    callbacks: {
+      onTrainBegin: () => console.log("Training Started"),
+      onTrainEnd: () => console.log("Training Completed"),
+      onEpochEnd: async(num, logs) => {
+        console.log("Epoch: " + num);
+        console.log("Loss: " + logs.loss);
+      }
+    }
+  };
+
+  return await model.fit(xs, ys, options);
+}
+
+function handleButtons(){
+  let trainButton = createButton("Train");
+  let testButton = createButton("Test");
+
+  trainButton.mouseClicked(() => {
+    console.log("bor");
+  });
+  testButton.mousePressed(() => {
+    console.log("boleta");
+  });
 }
 
 function setup() {
@@ -39,6 +90,12 @@ function setup() {
   background(0);
   cleanData(trainData);
 
+  handleButtons();
+
+  createModel();
+  // trainModel();
+
+  //console.log(xs.shape);
   // const example = tf.tensor2d(values, shape, 'int32');
   // example.dispose();
 }
