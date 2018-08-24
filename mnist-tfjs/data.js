5d284d8aff05c354183bbb050ea0937174ebb4b1
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-24 14:58:21 -0300

    Refactoring data functions

---
+++ mnist-tfjs/data.js
@@ -0,0 +1,65 @@
+function cleanData(data){
+  let values = [];
+  let labels = [];
+
+  for (let i = 0; i < data.rows.length; i++){
+    let number = data.rows[i];
+    labels.push(number.arr.shift());
+    values.push(number.arr);
+  }
+
+  let labelsTensor = tf.tensor1d(labels, 'int32');
+  const xs = tf.tensor2d(values, [DATA_AMOUNT, IMG_SIZE] ,'int32');
+  const ys = tf.oneHot(labelsTensor, 10);
+
+  labelsTensor.dispose();
+
+  return {xs, ys};
+}
+
+//TODO -> [3,3,10] conv, [1,1,1] conv, [,256] dense, [,10] dense, batch size 56, epochs 13
+function createModel(){
+  model = tf.sequential();
+
+  const hidden = tf.layers.dense({
+    inputDim: 784,
+    units: 3,
+    activation: 'sigmoid'
+  });
+
+  const output = tf.layers.dense({
+    units: 10,
+    activation: 'softmax'
+  });
+
+  model.add(hidden);
+  model.add(output);
+
+  const LEARNING_RATE = 0.03;
+  const opt = tf.train.sgd(LEARNING_RATE);
+
+  const config = {
+    optimizer: opt,
+    loss: 'categoricalCrossentropy'
+  };
+
+  model.compile(config);
+}
+
+async function trainModel(data){
+  const options = {
+    epochs: 12,
+    shuffle: true,
+    batchSize: 60,
+    callbacks: {
+      onTrainBegin: () => console.log("Training Started"),
+      onTrainEnd: () => console.log("Training Completed"),
+      onEpochEnd: async(num, logs) => {
+        console.log("Epoch: " + num);
+        console.log("Loss: " + logs.loss);
+      }
+    }
+  };
+
+  return await model.fit(data.xs, data.ys, options);
+}
--- mnist-tfjs/sketch.js
+++ mnist-tfjs/sketch.js
@@ -1,89 +1,20 @@
-let testFile, trainFile;
 let model;
 
+let testFile, trainFile;
 let trainData, testData;
 
 let trainButton, testButton;
-
+let DATA_AMOUNT;
 const IMG_SIZE = 784;
-const DATA_AMOUNT = 10000;
+
 
 function preload() {
-  // testData = loadTable("zdata/example.csv", "csv", "header");
-  trainFile = loadTable("zdata/train10001.csv", 'csv', 'header');
-  testData = loadTable("zdata/test10001.csv", 'csv', 'header');
+  trainFile = loadTable("zdata/train1001.csv", 'csv', 'header');
+  // testFile = loadTable("zdata/test1001.csv", 'csv', 'header');
+  testFile = trainFile;
+  console.log(trainFile);
 }
 
-function cleanData(data){
-  let values = [];
-  let labels = [];
-
-  for (let i = 0; i < data.rows.length; i++){
-    let number = data.rows[i];
-    labels.push(number.arr.shift());
-    values.push(number.arr);
-  }
-
-  let labelsTensor = tf.tensor1d(labels, 'int32');
-  const xs = tf.tensor2d(values, [DATA_AMOUNT, IMG_SIZE] ,'int32');
-  const ys = tf.oneHot(labelsTensor, 10);
-
-  labelsTensor.dispose();
-
-  return {xs, ys};
-}
-
-//TODO -> [3,3,10] conv, [1,1,1] conv, [,256] dense, [,10] dense, batch size 56, epochs 13
-function createModel(){
-  model = tf.sequential();
-
-  const hidden = tf.layers.dense({
-    inputDim: 784,
-    units: 3,
-    activation: 'sigmoid'
-  });
-
-  const output = tf.layers.dense({
-    units: 10,
-    activation: 'softmax'
-  });
-
-  model.add(hidden);
-  model.add(output);
-
-  const LEARNING_RATE = 0.03;
-  const opt = tf.train.sgd(LEARNING_RATE);
-
-  const config = {
-    optimizer: opt,
-    loss: 'categoricalCrossentropy'
-  };
-
-  model.compile(config);
-}
-
-async function trainModel(data){
-  const options = {
-    epochs: 12,
-    shuffle: true,
-    batchSize: 60,
-    callbacks: {
-      onTrainBegin: () => console.log("Training Started"),
-      onTrainEnd: () => console.log("Training Completed"),
-      onEpochEnd: async(num, logs) => {
-        console.log("Epoch: " + num);
-        console.log("Loss: " + logs.loss);
-      }
-    }
-  };
-
-  return await model.fit(data.xs, data.ys, options);
-}
-
-function testModel(data){
-  const result = model.evaluate(data.xs, data.ys);
-  result.print();
-}
 
 function handleButtons(){
   trainButton.mouseClicked(() => {
@@ -92,21 +23,26 @@ function handleButtons(){
 
   testButton.mousePressed(() => {
     console.log("Testing");
-    testModel(testData);
+    const result = model.evaluate(testData.xs, testData.ys, {batchSize: 30});
+    result.print();
   });
 }
 
 function setup() {
   createCanvas(200, 200);
   background(0);
+  DATA_AMOUNT = trainFile.rows.length;
 
   createModel();
   trainData = cleanData(trainFile);
+
+  //TODO -> fix train data without label
+  
+  // testData = cleanData(trainFile);
   testData = trainData;
 
   trainButton = createButton("Train");
   testButton = createButton("Test");
-
 }
 
 function draw() {
